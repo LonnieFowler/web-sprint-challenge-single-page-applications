474caa4d09eda13cf63d8e8fe4e7642b9228c501
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-02-25 13:38:24 -0700

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import {Route, Switch, Link} from 'react-router-dom'
+
+
+
+
 
 const App = () => {
+  
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <header>Lambda Eats
+      <Link to='/'>Home</Link>
+
+        <Link to='/pizza'>Help</Link>
+      </header>
+      <Switch>
+        <Route exact path='/'>
+        <h1>Your favorite food delivered while coding</h1>
+        <Link id="order-pizza" to='/pizza'>Order Now</Link>
+        </Route>
+        <Route path='/pizza'>
+          <h2>LOLOLOLOL</h2>
+          
+        </Route>
+
+      </Switch>
+     
+    </div>
   );
 };
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useHistory} from 'react-router-dom'
+
+export default function Home() {
+    const history = useHistory();
+
+}
+
+
+
+
+
+
+
+
