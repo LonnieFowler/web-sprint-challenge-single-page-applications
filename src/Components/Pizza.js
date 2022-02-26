b7ab43dc702c8bf880b8fc0560715a8a2232ed8b
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-02-25 17:59:26 -0700

    problems with yup

--- src/Components/Pizza.js
+++ src/Components/Pizza.js
@@ -1,45 +1,52 @@
 import React, {useState, useEffect} from'react';
-import {useParams} from "react-router-dom";
-
+import axios from 'axios';
 
 const OrderForm = () => {
 
     const [form, setForm] = useState({
        name:'',
+       size: '',
+       topping1: false,
+       topping2: false,
+       topping3: false,
+       topping4: false,
+       special: ''
     });
 
-//     const handleSubmit = () => {
-//         axios.post('https://reqres.in/api/orders', formValues)
-//         .then(res => {
-//           setUsers([res.data, ...users])
+    const onSubmit = () => {
+        axios.post('https://reqres.in/api/orders', form)
+            .then(res => {
+            console.log;
           
-//      })
-//         .catch(err => console.error(err));
-//      }
+      }).catch(err => console.error(err))
+      }
 
-//     const handleChange =(name,value) => {
-//     validate(name, value);
-//     setFormValues({...formValues, [name]:value});
-//   }
-
-//     const onChange = (evt) => {
-//         const {name, value, checked, type} = evt.target;
-//         const newValue = type ==='checkbox' ? checked : value;
-//         change(name, newValue)
-//     }
+      const onChange = (evt) => {
+        const value = evt.target.type ==='checkbox' ? evt.target.checked : evt.target.value;
+        setForm({
+            ...form,
+        [evt.target.name]: value
+  });
+      }
     
     return(<article>
     <h1>Pizza Order Form</h1>
-    <form id='pizza-form'>
+    <form id='pizza-form' onSubmit={onSubmit}>
         <label>Enter your name
         <input id='name-input'
         type='text' 
-        name=''/>
+        name='name'
+        value={form.name.value}
+        onChange={onChange}
+        />
         </label>
         <label>Select Size
             <select id='size-dropdown'
             type=''
-            name=''>
+            name='size'
+            value={form.size.value}
+            onChange={onChange}
+            >
                 <option value="12">12"</option>
                 <option value="16">16"</option>
                 <option value="22">22"</option>
@@ -47,22 +54,40 @@ const OrderForm = () => {
             </select>
         </label>
         <label>Pepperoni
-            <input type='checkbox' name=''/>
+            <input type='checkbox' 
+            name='topping1'
+            onChange={onChange}
+            checked={form.topping1}
+            />
         </label>
         <label>Mushroom
-            <input type='checkbox' name=''/>
+            <input type='checkbox' 
+            name='topping2'
+            onChange={onChange}
+            checked={form.topping2}
+            />
         </label>
         <label>Green Pepper
-            <input type='checkbox' name=''/>
+            <input type='checkbox' 
+            name='topping3'
+            onChange={onChange}
+            checked={form.topping3}
+            />
         </label>
         <label>Onion
-            <input type='checkbox' name=''/>
-        </label>
-        <label>Extra Cheeze
-            <input type='checkbox' name=''/>
+            <input type='checkbox' 
+            name='topping4'
+            onChange={onChange}
+            checked={form.topping4}
+            />
         </label>
         <label>
-            <input id='special-text' type='text' name=''/>
+            <input id='special-text' 
+            type='text' 
+            name='special'
+            value={form.special.value}
+            onChange={onChange}
+            />
         </label>
         <button id="order-button" type='submit'>Submit</button>
     </form>
